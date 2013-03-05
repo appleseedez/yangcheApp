881939132ecfe67090bcb1bdd51fc80638e574c4
appleseedez <appleseedez@gamil.com>
2013-03-05 16:23:06 +0800

    搜索功能基本实现,差自动补全

--- YCGlobalSearchViewController.h
+++ YCGlobalSearchViewController.h
@@ -2,12 +2,13 @@
 //  YCGlobalSearchViewController.h
 //  yangcheApp
 //
-//  Created by Pharaoh on 13-2-26.
+//  Created by Pharaoh on 13-3-5.
 //  Copyright (c) 2013年 com.yangche.app. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface YCGlobalSearchViewController : UIViewController <UISearchBarDelegate>
+@interface YCGlobalSearchViewController : UITableViewController <UISearchBarDelegate,UISearchDisplayDelegate>
+@property (weak, nonatomic) IBOutlet UISearchBar *globalSearchBar;
 
 @end
--- YCGlobalSearchViewController.m
+++ YCGlobalSearchViewController.m
@@ -2,21 +2,26 @@
 //  YCGlobalSearchViewController.m
 //  yangcheApp
 //
-//  Created by Pharaoh on 13-2-26.
+//  Created by Pharaoh on 13-3-5.
 //  Copyright (c) 2013年 com.yangche.app. All rights reserved.
 //
 
 #import "YCGlobalSearchViewController.h"
 
-@interface YCGlobalSearchViewController ()
-@property(weak,nonatomic) IBOutlet UISearchBar* searchBar;
+@interface YCGlobalSearchViewController (){
+	NSMutableArray* defaultResults;
+	NSMutableArray* fileterKeys;
+	//UISearchBar* globalSearchBar;
+	//UISearchDisplayController* searchDC;
+}
+
 @end
 
 @implementation YCGlobalSearchViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
     }
@@ -26,10 +31,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-	[self.searchBar setShowsCancelButton:NO animated:YES];
+	//globalSearchBar = [[NSBundle mainBundle] loadNibNamed:@"GlobalSearchBarView" owner:self options:nil][0];
+	//globalSearchBar.delegate = self;
 	UIImage *bg = [[UIImage imageNamed:@"nav_bg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
 	[[UISearchBar appearance] setBackgroundImage:bg];
+	//self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:globalSearchBar];
+	//searchDC = self.searchDisplayController;
+	self.globalSearchBar.scopeButtonTitles = @[@"全部",@"服务",@"店铺"];
+	//searchDC.delegate = self;
+	//searchDC.searchResultsDelegate = self;
+	//searchDC.searchResultsDataSource = self;
+	defaultResults = [@[@"One",@"Two",@"Three"] mutableCopy];
+	self.globalSearchBar.showsCancelButton = NO;
+	
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,13 +52,68 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    // Return the number of rows in the section.
+    return [defaultResults count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (nil == cell) {
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+	}
+    cell.textLabel.text = defaultResults[indexPath.row];
+    return cell;
+}
+
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+
+#pragma mark - UISearchBar delegate
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+	[searchBar resignFirstResponder];
+	searchBar.showsScopeBar = NO;
+	[self.searchDisplayController setActive:NO animated:YES];
+	
+}
+
 - (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar{
-	[searchBar setShowsCancelButton:YES animated:YES];
+	searchBar.showsScopeBar =YES;
+	[self.searchDisplayController setActive:YES animated:YES];
+	searchBar.showsCancelButton = NO;
 	return YES;
 }
 
-- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
-	[searchBar resignFirstResponder];
-    [searchBar setShowsCancelButton:NO animated:YES];
-}
+
+
+
+#pragma mark - UISearchBarDisplayController delegate
+
 @end
